85b4ebf82235e1fae8fe7fb41f1fbe41836a2fda
Muco Rolle Tresor <mucyotreasure@gmail.com>
2019-10-31 18:33:37 +0200

    setup typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductModule } from './modules/product/product.module';
 
 @Module({
     imports: [
+        TypeOrmModule.forRoot(),
         GraphQLModule.forRoot({
             autoSchemaFile: './src/schema.graphql',
             debug: true,
